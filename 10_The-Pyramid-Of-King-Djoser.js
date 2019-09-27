7f529b03b1f5d460b513e5fffc8ee956ce6e0d06
Vasil Varshilov <40742216+VasilVarshilov@users.noreply.github.com>
2019-09-27 15:50:31 +0300

    Update 10_The-Pyramid-Of-King-Djoser.js

--- 10_The-Pyramid-Of-King-Djoser.js
+++ 10_The-Pyramid-Of-King-Djoser.js
@@ -1,38 +1,38 @@
-function solve(base, increment) {
-    base = parseFloat(base);
-    increment = parseFloat(increment);
-    let totalHeight = 0;
-    let stone = 0;
-    let marble = 0;
-    let lapisLazuli = 0;
-    let gold = 0;
-    let counter = 0;
+function solve(base, inc){
 
-    while (base >=1 ) {
-        counter += 1;
-        let outerLayer = 0;
-        let total = base * base;
-        if (counter % 5 === 0) {
-            outerLayer = (4 * base - 4);
-            lapisLazuli += outerLayer;
-            stone += (total - outerLayer);
-        } else if (base <= 2) {
-            gold = total;
-        } else {
-            outerLayer = 4 * base - 4;
-            marble += outerLayer;
-            stone += (total - outerLayer);
+    let stone, marble, lapis, gold, steps, totalHeight, stoneCount, marbelCount, lapisCount;
+    steps = 0;
+    stoneCount = 0;
+    marbelCount = 0;
+    lapisCount = 0;
+
+    for(let i=1; i = base; i++){
+        steps++;
+
+        if(base -2 <= 0){
+            gold = (base * base) * inc;
+            totalHeight = steps * inc;
+            break;
+        }
+
+        if(steps % 5 === 0 && base != 1){
+            stone = ((base - 2) * (base - 2)) * inc;
+            lapis = ((base*4)-4)*inc;
+            lapisCount+= lapis;
+            stoneCount+= stone;
+        }else{
+            stone = ((base - 2) * (base - 2) * inc);
+            marble = ((base*4)-4)*inc;
+            stoneCount+= stone;
+            marbelCount+= marble;
         }
         base -= 2;
+
     }
 
-    stone = Math.ceil(stone * increment);
-    marble = Math.ceil(marble * increment);
-    lapisLazuli = Math.ceil(lapisLazuli * increment);
-    gold = Math.ceil(gold * increment);
-    totalHeight = Math.floor(counter * increment);
-
-    console.log(`Stone required: ${stone}\nMarble required: ${marble}\nLapis Lazuli required: ${lapisLazuli}\nGold required: ${gold}\nFinal pyramid height: ${totalHeight}`);
-
+    console.log("Stone required: " + Math.ceil(stoneCount))
+    console.log("Marble required: " + Math.ceil(marbelCount))
+    console.log("Lapis Lazuli required: " + Math.ceil(lapisCount))
+    console.log("Gold required: " + Math.ceil(gold))
+    console.log("Final pyramid height: " + Math.floor(totalHeight))
 }
-solve(11, 0.75);
